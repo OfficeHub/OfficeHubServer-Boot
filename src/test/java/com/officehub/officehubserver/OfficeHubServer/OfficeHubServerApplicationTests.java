6434a150d1285f7f534fb2c6ce525dc02c59f5d6
zimin <wlals822@naver.com>
2021-03-12 15:47:06 +0900

    Initialize Spring boot server

---
+++ src/test/java/com/officehub/officehubserver/OfficeHubServer/OfficeHubServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.officehub.officehubserver.OfficeHubServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OfficeHubServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
