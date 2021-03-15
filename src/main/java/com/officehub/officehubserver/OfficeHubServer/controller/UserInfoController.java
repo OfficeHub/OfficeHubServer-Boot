a5bfefa0aa8c259cb781b14214e481c287f387f2
zimin <wlals822@naver.com>
2021-03-15 22:36:12 +0900

    Swagger 설정 추가
    관련 의존성 업데이트
    Swagger 설정을 초기화 하는 SpringFoxConfig.java 클래스 추가
    UserInfoController.java 에 주석을 추가해서 테스트
    
    이 커밋 이후부터 빌드시 https://localhost:8080/swagger-ui/ (마지막 / 주의) 로 접속하면 자동 생성된 문서 확인 가능

--- src/main/java/com/officehub/officehubserver/OfficeHubServer/controller/UserInfoController.java
+++ src/main/java/com/officehub/officehubserver/OfficeHubServer/controller/UserInfoController.java
@@ -2,6 +2,8 @@ package com.officehub.officehubserver.OfficeHubServer.controller;
 
 import com.officehub.officehubserver.OfficeHubServer.UserNotFoundException;
 import com.officehub.officehubserver.OfficeHubServer.dto.UserInfo;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,8 +11,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController()
 public class UserInfoController {
 
+    @Operation(summary = "id 기준으로 유저의 정보를 쿼리합니다.")
     @GetMapping("/users")
-    public UserInfo getUserInfo(@RequestParam(value = "id", defaultValue = "0") int id) {
+    public UserInfo getUserInfo(
+            @Parameter(description = "대상 유저의 id")
+            @RequestParam(value = "id", defaultValue = "0") int id) {
         if (id == 0) {
             return new UserInfo("zimin", "wlals822@naver.com");
         } else if (id == 1) {
