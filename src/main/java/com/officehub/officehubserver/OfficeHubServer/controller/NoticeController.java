5dfa71021ac2c2f8b115a4b2fc009d25ecfbc553
KIM-KYOUNG-OH <66231761+KIM-KYOUNG-OH@users.noreply.github.com>
2021-03-28 18:13:22 +0900

    공지 테스트 api

--- src/main/java/com/officehub/officehubserver/OfficeHubServer/controller/NoticeController.java
+++ src/main/java/com/officehub/officehubserver/OfficeHubServer/controller/NoticeController.java
@@ -1,12 +1,18 @@
 package com.officehub.officehubserver.OfficeHubServer.controller;
 
 import com.officehub.officehubserver.OfficeHubServer.dto.NoticeDto;
+import com.officehub.officehubserver.OfficeHubServer.repository.NoticeRepository;
+import com.officehub.officehubserver.OfficeHubServer.repository.TestRepository;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import lombok.Data;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.util.ArrayList;
+import java.text.ParseException;
 import java.util.List;
 
 @Controller
@@ -21,30 +27,16 @@ public class NoticeController {
         }
     }
 
-    @GetMapping("/notice")
+    @ApiOperation(value = "공지목록 출력", notes = "총 테스트 데이터 : 5개")
+    @GetMapping("test/notice")
     @ResponseBody
-    public JsonNoticeList getNoticeList(){
-        List<NoticeDto> list = new ArrayList<NoticeDto>();
-        NoticeDto ndto1 = new NoticeDto();
-        NoticeDto ndto2 = new NoticeDto();
-        NoticeDto ndto3 = new NoticeDto();
+    public JsonNoticeList getNoticeList(@ApiParam(value = "시작 순서", required = false, example = "2") @RequestParam(value = "offset", defaultValue = "0") int offset,
+                                        @ApiParam(value = "공지 총개수", required = false, example = "3") @RequestParam(value = "size", defaultValue = "5") int size) throws ParseException {
 
-        ndto1.setNoticeId(1);
-        ndto1.setTitle("코로나 공지사항");
-        ndto1.setContent("코로나 때문에 전체 직원 재택근무를 시행하겠습니다.");
-        list.add(ndto1);
+        NoticeRepository testRepository = new TestRepository();
 
-        ndto2.setNoticeId(2);
-        ndto2.setTitle("사무실 확장 공 공지사항");
-        ndto2.setContent("다음주 사무실 확장 공사가 예정되있습니다.");
-        list.add(ndto2);
+        return new JsonNoticeList(testRepository.getNotices(offset, size));
 
-        ndto3.setNoticeId(3);
-        ndto3.setTitle("주차시설 공지사항");
-        ndto3.setContent("주차시설 확장 공사로 인해 일주일간 사용을 금지합니다.");
-        list.add(ndto3);
-
-        return new JsonNoticeList(list);
     }
 }
 
---
+++ src/main/java/com/officehub/officehubserver/OfficeHubServer/repository/TestRepository.java
@@ -0,0 +1,81 @@
+package com.officehub.officehubserver.OfficeHubServer.repository;
+
+import com.officehub.officehubserver.OfficeHubServer.dto.NoticeDto;
+
+import java.sql.Timestamp;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class TestRepository implements NoticeRepository{
+    @Override
+    public List<NoticeDto> getNotices(int offset, int size) throws ParseException {
+
+        List<NoticeDto> list = new ArrayList<>();
+        NoticeDto ndto1 = new NoticeDto();
+        NoticeDto ndto2 = new NoticeDto();
+        NoticeDto ndto3 = new NoticeDto();
+        NoticeDto ndto4 = new NoticeDto();
+        NoticeDto ndto5 = new NoticeDto();
+
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+
+        ndto1.setNoticeId(1);
+        ndto1.setWriterId(1);
+        ndto1.setTitle("공지사항1");
+        ndto1.setContent("코로나 때문에 전체 직원 재택근무를 시행하겠습니다.");
+        String d1 = "2021-03-01 10:00:00";
+        Date date1 = sdf.parse(d1);
+        ndto1.setWriteday(date1);
+        list.add(ndto1);
+
+        ndto2.setNoticeId(2);
+        ndto2.setWriterId(1);
+        ndto2.setTitle("공지사항2");
+        ndto2.setContent("코로나 때문에 전체 직원 월급 삭갑.");
+        String d2 = "2021-03-05 11:00:00";
+        Date date2 = sdf.parse(d2);
+        ndto2.setWriteday(date2);
+        list.add(ndto2);
+
+        ndto3.setNoticeId(3);
+        ndto3.setWriterId(1);
+        ndto3.setTitle("공지사항3");
+        ndto3.setContent("코로나 때문에 전체 직원 해고.");
+        String d3 = "2021-03-02 12:00:00";
+        Date date3 = sdf.parse(d3);
+        ndto3.setWriteday(date3);
+        list.add(ndto3);
+
+        ndto4.setNoticeId(4);
+        ndto4.setWriterId(1);
+        ndto4.setTitle("공지사항4");
+        ndto4.setContent("코로나 너무 싫다");
+        String d4 = "2021-03-10 13:30:00";
+        Date date4 = sdf.parse(d4);
+        ndto4.setWriteday(date4);
+        list.add(ndto4);
+
+        ndto5.setNoticeId(5);
+        ndto5.setWriterId(1);
+        ndto5.setTitle("공지사항5");
+        ndto5.setContent("무야호");
+        String d5 = "2021-03-01 15:00:00";
+        Date date5 = sdf.parse(d5);
+        ndto5.setWriteday(date5);
+        list.add(ndto5);
+
+        List<NoticeDto> result = new ArrayList<>();
+
+        try {
+            for(int i=offset; i<(offset+size); i++){
+                result.add(list.get(i));
+            }
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+}
