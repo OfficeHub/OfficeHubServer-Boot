dafa644999201be50d22e7772ca700b15986fd21
CASY82 <repaid11010@gmail.com>
2021-04-25 15:48:33 +0900

    UserInfo DB connect

--- src/main/java/com/officehub/officehubserver/OfficeHubServer/UserInfoTest.java
+++ src/main/java/com/officehub/officehubserver/OfficeHubServer/UserInfoTest.java
@@ -11,9 +11,7 @@ public class UserInfoTest implements UserInfoInterface{
             @RequestParam(value = "id", defaultValue = "0") int id) throws Exception {
         if (id == 0) {
         	//현재 로그인된 사용자 == DB에 저장된 유저 id값 비교 후, 유저 데이터를 가져오도록 한다
-            return new UserInfo(id, "gildong.hong", "정보전략부", "010-1234-5678", "안녕하세요. 여기는 테스트 데이터", "여기는 사진 경로");
-//        } else if (id == 1) {
-//            return new UserInfo(id, "gildong.kim", "영업부", "010-1111-2222", "안녕하세요. 처음입니다2.", "여기는 사진 경로2");
+            return new UserInfo(id, "gildong.hong_테스트", "정보전략부", "010-1234-5678", "여기는 사진 경로");
         } else {
             throw new Exception();
         }
--- src/main/java/com/officehub/officehubserver/OfficeHubServer/controller/UserInfoController.java
+++ src/main/java/com/officehub/officehubserver/OfficeHubServer/controller/UserInfoController.java
@@ -1,37 +1,39 @@
 package com.officehub.officehubserver.OfficeHubServer.controller;
 
-import com.officehub.officehubserver.OfficeHubServer.UserInfoDatabase;
 import com.officehub.officehubserver.OfficeHubServer.UserInfoTest;
 import com.officehub.officehubserver.OfficeHubServer.dto.UserInfo;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
+import com.officehub.officehubserver.OfficeHubServer.service.UserInfoService;
 
+import io.swagger.annotations.ApiParam;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController()
 public class UserInfoController {
 	
-	//이제 id를 현재 로그인된 유저의 데이터로 가져오면 됨(쿠키, 세션 데이터 필요)
-	private int id = 0;
+	private final UserInfoService userService;
 	
+	public UserInfoController(UserInfoService userService) {
+		this.userService = userService;
+	}
 	
-	//이 부분은 web.config같은 따로 설정을 넣어서 수정이 가능하도록 하거나 추후엔 삭제 예정
-	private String status = "Test";
-	String DBside = "DB";
-	String Testside = "Test";
 	
 	@Operation(summary = "id 기준으로 유저의 정보를 쿼리합니다.")
-	@Parameter(description = "대상 유저의 id")
-    @GetMapping("/users")
-    public UserInfo UserData() throws Exception {
-        if (status == DBside) {
-            UserInfoDatabase DBstatus = new UserInfoDatabase();
-            return DBstatus.getUserInfo(id);
-        } else if (status == Testside) {
-        	UserInfoTest DBstatus = new UserInfoTest();
-        	return DBstatus.getUserInfo(id);
-        } else {
+	 @GetMapping("/userinfo")
+    public UserInfo UserData(@ApiParam(value = "사번", example = "0")
+    						 @RequestParam(value = "id")int id) {
+        return userService.getUserInfo(id);
+    }
+	
+	@Operation(summary = "테스트 완성 후 삭제할 것")
+	@GetMapping("/test/users")
+    public UserInfo UserData_test() throws Exception {
+        try{
+            UserInfoTest getUser = new UserInfoTest();
+        	return getUser.getUserInfo(0);
+        } catch(Exception e){
             throw new Exception();
         }
     }
